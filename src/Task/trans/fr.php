9fd1e9f22352e705dc1544f428d4284d198c2953
Martindev <Delebecque.martin@gmail.com>
2022-01-25 00:44:47 +0100

    Update fr.php

--- src/Task/trans/fr.php
+++ src/Task/trans/fr.php
@@ -4,7 +4,7 @@ return [
         "title" => "Tâches",
         "subtitle" => "Gérez les tâches du système.",
         "operational" => "Tout les systèmes sont opérationnels.",
-        "notoperational" => "Certains systèmes rencontres des problèmes.",
+        "notoperational" => "Certains systèmes rencontrent des problèmes.",
         "category" => "Catégorie",
         "progress" => "Progression",
         "content" => "Contenu détaillés",
