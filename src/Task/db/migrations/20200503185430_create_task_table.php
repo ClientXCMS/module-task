5c62d37a58e353655dca39e54c63e8c5349a75fe
Martindev <Delebecque.martin@gmail.com>
2022-02-14 01:15:43 +0100

    Update 20200503185430_create_task_table.php

--- src/Task/db/migrations/20200503185430_create_task_table.php
+++ src/Task/db/migrations/20200503185430_create_task_table.php
@@ -7,7 +7,7 @@ class CreateTaskTable extends AbstractMigration
     public function change()
     {
         $task = $this->table('tasks');
-        if ($task->exists()){
+        if (!$task->exists()){
             $task->addColumn('name', 'string')
             ->addColumn('server_id', 'integer')
             ->addColumn('progress', 'integer', ['default' => 1])
@@ -16,12 +16,12 @@ class CreateTaskTable extends AbstractMigration
             ->addColumn('close_at', 'datetime', ['null' => true])
             ->addColumn('state', 'string', ['default' => \App\Task\Entity\Task::OPEN])
             ->addTimestamps()
-            ->addForeignKey('server_id', 'servers', ['delete' => 'CASCADE'])
+            ->addForeignKey('server_id', 'servers', ['id'], ['delete' => 'CASCADE'])
             ->create();
         }
         $comments = $this->table('tasks_comments');
 
-        if ($comments->exists()){
+        if (!$comments->exists()){
         $comments
             ->addColumn('content', 'string')
             ->addColumn('task_id', 'integer', ['null' => true])
