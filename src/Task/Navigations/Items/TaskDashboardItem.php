1c1c8d16ded726ded6deaf7adc6ee4607c4a835a
Martindev <Delebecque.martin@gmail.com>
2022-02-26 10:30:37 +0100

    Update TaskDashboardItem.php

--- src/Task/Navigations/Items/TaskDashboardItem.php
+++ src/Task/Navigations/Items/TaskDashboardItem.php
@@ -1,8 +1,11 @@
+        $first = collect($data)->first(null, ["lastping" => time()]);
 <?php
 
 
 namespace App\Task\Navigations\Items;
 
+use App\Task\Database\TaskTable;
+use App\Task\Entity\Task;
 use App\Task\TaskPing;
 use Carbon\Carbon;
 use ClientX\Navigation\NavigationItemInterface;
@@ -12,11 +15,16 @@ use ClientX\Translator\Translater;
 class TaskDashboardItem implements NavigationItemInterface
 {
     private Translater $translater;
+    /**
+     * @var \App\Task\Database\TaskTable
+     */
+    private TaskTable $table;
 
 
-    public function __construct(Translater $translater)
+    public function __construct(Translater $translater, TaskTable $table)
     {
         $this->translater = $translater;
+        $this->table = $table;
     }
 
 
@@ -33,6 +41,8 @@ class TaskDashboardItem implements NavigationItemInterface
      */
     public function render(RendererInterface $renderer): string
     {
+
+        $tasks = $this->table->fetchAll();
         $data = (new TaskPing())->getFromSource();
         $data = collect($data)->sortByDesc(function ($data) {
             return $data['online'] === false;
@@ -40,11 +50,15 @@ class TaskDashboardItem implements NavigationItemInterface
         $ok = collect($data)->filter(function ($line) {
                 return $line['online'];
         })->count() === count($data);
-        if (count($data) === 0) {
+        if (count($tasks) == collect($tasks)->filter(function (Task $task) {
+                return $task->getBullet() == 'yes';
+        })->count()) {
+            $ok = true;
+        } else {
             $ok = false;
         }
-        
-        $first = collect($data)->first(null,['lastping' => time()]);
+        $first = collect($data)->first(null, ["lastping" => time()]);
+
         Carbon::setLocale(explode('_', $this->translater->getLocale(), 2)[0]);
         $lastUpdated = Carbon::createFromTimestamp($first['lastping'])->diffForHumans();
         return $renderer->render("@task_admin/dashboard", compact('data', 'ok', 'lastUpdated'));
