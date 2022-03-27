92e0386fde4c6945582bea6e4415dde307b0e8f2
Martindev <Delebecque.martin@gmail.com>
2022-03-27 03:25:31 +0200

    Fix permissions

--- src/Task/config.php
+++ src/Task/config.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Task\Actions\TaskCrudAction;
 use App\Task\Navigations\Items\TaskAdminItem;
-
+use App\Task\Navigations\Items\TaskDashboardItem;
 use App\Task\TaskSchedule;
 use ClientX\Navigation\DefaultMainItem;
 use function DI\add;
@@ -10,6 +11,11 @@ use function DI\get;
 return [
     'admin.menu.items' => add([get(TaskAdminItem::class)]),
     "cron.schedules" => add(TaskSchedule::class),
-    'admin.dashboard.items' => add(get(\App\Task\Navigations\Items\TaskDashboardItem::class)),
+    'admin.dashboard.items' => add(get(TaskDashboardItem::class)),
     'navigation.main.items' => add(new DefaultMainItem([DefaultMainItem::makeItem("task.title", "task", "fa fa-server", true, true)], 40)),
+    'permissions.list' => add([
+        TaskCrudAction::class => 'task',
+        'task.admin.comment' => "Post comments",
+        'task.admin.delete' => "Delete comments"
+    ])
 ];
